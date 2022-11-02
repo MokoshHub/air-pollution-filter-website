18e568e98f64aabdbf30171b51f99bfbd55dbe24
saras <lost.func@gmail.com>
2022-11-02 19:49:30 +0100

    latest website design

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,17 @@ module.exports = {
     fontFamily: {
       'sans': ['Roboto', 'sans-serif'],
     },
-    extend: {
-      backgroundImage: {
-        'mokosh': "url('/static/assets/bg.jpg')",
-      }
+    colors: {
+      'background': '#423629',
+      'light-text': '#DEE3F1',
+      'dark-text': '#423629',
+      'panel-background': '#7D7E75',
+      'content-background': '#DEE3F1',
+      'panel-buttons': '#4F5D2F',
+      'button': '#4F5D2F',
+      'button-hover': '#7D7E75',
+      'footer': '#7D7E75'
     },
+    extend: {},
   }
 }
