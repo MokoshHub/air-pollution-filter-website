e750bfdcff17e74736f55a2bc50c05cb10085395
saras <lost.func@gmail.com>
2022-11-17 11:25:09 +0100

    POST + timestamp info

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, timestamp} from 'rxjs';
 import { AppComponent } from '../app.component';
 
 @Injectable({
@@ -17,6 +17,19 @@ export class FileUploadService {
         formData.append("lat", lat);
         formData.append("lon", lon);
 
+        var unix_time = Math.round(+new Date()/1000);
+        var timestamp = this.convert_time(unix_time);
+
+        formData.append("timestamp", timestamp.toString());
+
         return this.http.post(AppComponent.baseUrl, formData, {responseType: 'text'})
+    }
+
+    convert_time (unix_time) {
+        var epoch = (unix_time * 1000);
+        var date = new Date();
+        var localOffset = (-1) * date.getTimezoneOffset() * 60000;
+        var stamp = Math.round(new Date(epoch + localOffset).getTime());
+        return stamp;
     }
 }
